07e723cfb3a3fc1d11fd8df7a9a3c97d440d83f9
GeorginaXu <cxu@smith.edu>
2019-02-09 14:54:24 -0500

    list file functionality

--- src/main/java/edu/smith/cs/csc262/coopsh/apps/ListFiles.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/ListFiles.java
@@ -1,4 +1,28 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
-public class ListFiles {
+import edu.smith.cs.csc262.coopsh.ShellEnvironment;
+import edu.smith.cs.csc262.coopsh.Task;
+
+import java.io.File;
+
+public class ListFiles extends Task {
+    /**
+     * All tasks are created with a possibly empty list of string arguments!
+     *
+     * @param env
+     * @param args - much like public static void main!
+     */
+    public ListFiles(ShellEnvironment env, String[] args) {
+        super(env, args);
+    }
+
+    @Override
+    protected void update() {
+        File path = env.currentDirectory;
+        for (String file: path.list()) {
+            this.println(file);
+        }
+        this.closeOutput();
+        this.exit(0);
+    }
 }

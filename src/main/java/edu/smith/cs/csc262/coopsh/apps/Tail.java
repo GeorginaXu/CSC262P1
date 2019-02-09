7b2f7b6778ca914baa67409cdeab74b3026eea06
GeorginaXu <cxu@smith.edu>
2019-02-09 16:58:58 -0500

    tail functionality

--- src/main/java/edu/smith/cs/csc262/coopsh/apps/Tail.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/Tail.java
@@ -1,4 +1,17 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
-public class Tail {
+import edu.smith.cs.csc262.coopsh.ShellEnvironment;
+import edu.smith.cs.csc262.coopsh.Task;
+
+public class Tail extends Task {
+
+    public Tail(ShellEnvironment env, String[] args) {
+        super(env, args);
+    }
+
+    @Override
+    protected void update(){
+
+    }
 }
+

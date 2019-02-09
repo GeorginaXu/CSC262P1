9a60d84edb7ad489a27300759a49a95d49f576df
GeorginaXu <cxu@smith.edu>
2019-02-09 16:55:39 -0500

    sort functionality

--- src/main/java/edu/smith/cs/csc262/coopsh/apps/Sort.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/Sort.java
@@ -1,4 +1,35 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
-public class Sort {
+import edu.smith.cs.csc262.coopsh.ShellEnvironment;
+import edu.smith.cs.csc262.coopsh.Task;
+import edu.smith.cs.csc262.coopsh.InputLine;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort extends Task {
+
+    /** List that will store all inputs individually, which makes it easier to sort. */
+    private List<String> inputs = new ArrayList<>();
+
+    public Sort(ShellEnvironment env, String[] args) {
+        super(env, args);
+    }
+
+    @Override
+    protected void update() {
+        InputLine current = this.input.poll();
+        if (current.isEndOfFile()) {
+            // Sort all inputs.
+            Collections.sort(inputs);
+            for (String str : inputs) {
+                this.println(str);
+            }
+            this.closeOutput();
+            this.exit(0);
+            return;
+        }
+        // if not end of file, add the input into the List.
+        inputs.add(current.get());
+    }
 }

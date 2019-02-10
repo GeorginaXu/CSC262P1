f301215384f35e3b160d0d1b2a4ef4f9052ae9e7
GeorginaXu <cxu@smith.edu>
2019-02-10 08:32:41 -0500

    set variable functionality

--- src/main/java/edu/smith/cs/csc262/coopsh/apps/SetVar.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/SetVar.java
@@ -1,4 +1,27 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
-public class SetVar {
+import edu.smith.cs.csc262.coopsh.ShellEnvironment;
+import edu.smith.cs.csc262.coopsh.Task;
+
+public class SetVar extends Task {
+
+    private ShellEnvironment env;
+    private String[] inputs;
+
+    public SetVar(ShellEnvironment env, String[] args) {
+        super(env, args);
+        this.inputs = args;
+        this.env = env;
+    }
+
+    @Override
+    protected void update(){
+        try {
+            env.setVariable(inputs[0], inputs[1]);
+        } catch (IndexOutOfBoundsException e) {
+            caughtFatalException("Please input two arguments!", e);
+        }
+        this.closeOutput();
+        this.exit(0);
+    }
 }
